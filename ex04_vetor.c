f6833af2551a86d401a971397d8aa4af4fe107ea
matheusoliveiranicolas-lang <matheus.oliveira.nicolas@escola.pr.gov.br>
2025-11-24 08:08:28 -0300

    Update ex04_vetor.c

--- ex04_vetor.c
+++ ex04_vetor.c
@@ -1,16 +1,1 @@
-# Vetor — Maior, menor e média (10 posições)
 
-numeros = []  # cria a lista (vetor) vazia
-
-for i in range(10):
-    num = int(input(f"Digite o {i+1}º número: "))
-    numeros.append(num)  # adiciona o número na lista
-
-maior = max(numeros)
-menor = min(numeros)
-media = sum(numeros) / len(numeros)
-
-print("\nResultados:")
-print(f"Maior número: {maior}")
-print(f"Menor número: {menor}")
-print(f"Média dos números: {media:.2f}")

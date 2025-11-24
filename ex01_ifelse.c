ffe6f590af87584dbd0c2fa30eba133016a9a694
matheusoliveiranicolas-lang <matheus.oliveira.nicolas@escola.pr.gov.br>
2025-11-24 07:51:08 -0300

    Add number classification for positive, negative, zero

--- ex01_ifelse.c
+++ ex01_ifelse.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main()
+{
+    int numero;
+   
+    printf("digite um numero:");
+    scanf("%d",&numero);
+   
+    if(numero>0){
+        printf("Positivo\n");
+    }
+    else if(numero<0){
+        printf("Negativo\n");
+    }
+    else{
+        printf ("zero\n");
+    }
+   
+    if(numero%2==0){
+        printf ("par");
+    }
+    else{
+      printf ("impar");
+    }
+
+    return 0;
+}

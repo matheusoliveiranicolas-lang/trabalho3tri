35d715ae428f2c3b4a59d0661e3216c16e9ed924
matheusoliveiranicolas-lang <matheus.oliveira.nicolas@escola.pr.gov.br>
2025-11-24 07:40:10 -0300

    Update ex01_ifelse.c

--- ex01_ifelse.c
+++ ex01_ifelse.c
@@ -1,17 +1,1 @@
-# If/else — Classificador de número
 
-numero = int(input("Digite um número inteiro: "))
-
-# Verifica positivo, negativo ou zero
-if numero > 0:
-    print("O número é positivo.")
-elif numero < 0:
-    print("O número é negativo.")
-else:
-    print("O número é zero.")
-
-# Verifica par ou ímpar
-if numero % 2 == 0:
-    print("O número é par.")
-else:
-    print("O número é ímpar.")
